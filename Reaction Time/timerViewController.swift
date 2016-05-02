d066f7bd667eb67558746804782ff6935b5be3a2
student1 <student1@jh-mbair-158-19.d214.org>
2016-05-02 09:58:21 -0500

    starting code NSTimer

--- Reaction Time/timerViewController.swift
+++ Reaction Time/timerViewController.swift
@@ -9,10 +9,34 @@
 import UIKit
 
 class timerViewController: UIViewController {
+    
+    var startTime = NSTimeInterval()
 
     @IBOutlet weak var displayTimer: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+
+    }
+    func updateTime(){
+        var currentTime = NSDate.timeIntervalSinceReferenceDate()
+        var elapsedTime: NSTimeInterval = currentTime - startTime
+        
+        
+        let minutes = UInt8(elapsedTime / 60.0)
+        elapsedTime -= (NSTimeInterval(minutes) * 60)
+        let seconds = UInt8(elapsedTime)
+        elapsedTime -= NSTimeInterval(seconds)
+        let fraction = UInt8(elapsedTime * 100)
+        
+        
+        let strMinutes = String(format: "%02d", minutes)
+        let strSeconds = String(format: "%02d", seconds)
+        let strFraction = String(format: "%02d", fraction)
+        
+        displayTime.text = “\(strMinutes),\(strSeconds),\(strFraction)”
+        
+    }
 
     }
 
