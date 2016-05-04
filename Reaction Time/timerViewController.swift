4e1e40cf76ddbef67135fe476606fa3afefd6d09
student1 <student1@jh-mbair-158-19.d214.org>
2016-05-04 09:58:17 -0500

    timer image finished

--- Reaction Time/timerViewController.swift
+++ Reaction Time/timerViewController.swift
@@ -14,21 +14,17 @@ class timerViewController: UIViewController {
     var timer = NSTimer()
     
     
+    @IBOutlet weak var tapStop: UIImageView!
     @IBOutlet weak var displayTimer: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        
     }
-    
+
     @IBAction func stop(sender: UIButton) {
-        if displayTimer.text == "00:01:00"{
-            
-        }
+        
         timer.invalidate()
     }
-    
-    
+
     @IBAction func start(sender: UIButton) {
         
         if (!timer.valid) {
@@ -37,23 +33,25 @@ class timerViewController: UIViewController {
             go = NSDate.timeIntervalSinceReferenceDate()
         }
     }
-    
     func updateTime(){
         var timeNow = NSDate.timeIntervalSinceReferenceDate()
         var pass: NSTimeInterval = timeNow - go
-        
-        
+
         let minutes1 = UInt8(pass / 60.0)
         pass -= (NSTimeInterval(minutes1) * 60)
         let seconds1 = UInt8(pass)
         pass -= NSTimeInterval(seconds1)
         let milli1 = UInt8(pass * 100)
         
-        
         let minutes2 = String(format: "%02d", minutes1)
         let seconds2 = String(format: "%02d", seconds1)
         let milli2 = String(format: "%02d", milli1)
         displayTimer!.text = "\(minutes2):\(seconds2):\(milli2)"
+        
+        if displayTimer.text == "00:01:00"{
+            tapStop.image = UIImage(named: "carcrash")
+        }
+
         
     }
     
