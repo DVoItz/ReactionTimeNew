f5970f3bff4812ecdf26d853589928aae66c8710
student1 <student1@jh-mbair-158-19.d214.org>
2016-05-06 10:13:14 -0500

    basically finished version

--- Reaction Time/passViewController.swift
+++ Reaction Time/passViewController.swift
@@ -12,13 +12,29 @@ class passViewController: UIViewController {
     
     var reactTime = String()
     
+    @IBOutlet weak var passFailImage2: UIImageView!
+    @IBOutlet weak var passFailImage: UIImageView!
+    @IBOutlet weak var passFailLabel2: UILabel!
     @IBOutlet weak var passFailLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
-        print(reactTime)
         var doubleTime = NSString(string: reactTime).doubleValue
+        print(doubleTime)
         passFailLabel.textColor = UIColor.whiteColor()
-        passFailLabel.text! = "Your Reaction Time was \(doubleTime - 1)"
+        passFailLabel.text! = "\(doubleTime - 1)"
+        
+        if doubleTime > 1.62 {
+            passFailLabel2.text! = "YOU FAILED"
+            passFailLabel2.textColor = UIColor.redColor()
+            passFailImage.image = UIImage(named: "fail")
+            passFailImage2.image = UIImage(named: "fail")
+        }
+        else {
+            passFailLabel2.text! = "YOU PASSED"
+            passFailLabel2.textColor = UIColor.yellowColor()
+            passFailImage.image = UIImage(named: "smiley")
+            passFailImage2.image = UIImage(named: "smiley")
+        }
 
     }
     
