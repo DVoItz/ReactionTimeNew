99e84b67cccd647718cff341fe8ccc4a7df879cd
student1 <student1@jh-mbair-158-13.d214.org>
2016-05-25 09:47:11 -0500

    changed the difficulty

--- Reaction Time/timerViewController.swift
+++ Reaction Time/timerViewController.swift
@@ -76,7 +76,7 @@ class timerViewController: UIViewController {
             go = NSDate.timeIntervalSinceReferenceDate()
             
             sender.highlighted = false
-            UIView.animateWithDuration(1.0, delay: 0, options: .CurveLinear, animations: { sender.alpha = 0 }, completion: nil)
+            UIView.animateWithDuration(0.8, delay: 0, options: .TransitionCurlDown, animations: { sender.alpha = 0 }, completion: nil)
         }
     }
     
