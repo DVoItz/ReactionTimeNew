9470608d5aa1fabfadcec29c5aaf9172d43896d5
student1 <student1@jh-mbair-158-19.d214.org>
2016-05-27 09:50:33 -0500

    lol

--- Reaction Time/AppDelegate.swift
+++ Reaction Time/AppDelegate.swift
@@ -16,7 +16,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         NSThread.sleepForTimeInterval(1.5)
-        return true
+        return true 
     }
 
     func applicationWillResignActive(application: UIApplication) {
